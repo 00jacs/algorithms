3552d1090b4cff318c0bd2ccac6a6bc98786f8e1
00jacs <gillbatesdev@gmail.com>
2024-09-08 06:37:04 +0200

    323: update solution.cpp formatting

--- leetcode/323-number-of-connected-components-in-an-undirected-graph/solution.cpp
+++ leetcode/323-number-of-connected-components-in-an-undirected-graph/solution.cpp
@@ -1,34 +1,31 @@
-class Solution {
-public:
-    void dfs(int root, vector<vector<int>>& graph, vector<bool>& seen) {
-        seen[root] = true;
+void dfs(int root, vector<vector<int>>& graph, vector<bool>& seen) {
+    seen[root] = true;
 
-        for (int i = 0; i < graph[root].size(); i++) {
-            // graph[root][i] is our neighbour
-            if (!seen[graph[root][i]]) {
-                dfs(graph[root][i], graph, seen);
-            }
+    for (int i = 0; i < graph[root].size(); i++) {
+        // graph[root][i] is our neighbour
+        if (!seen[graph[root][i]]) {
+            dfs(graph[root][i], graph, seen);
+        }
+    }
+}
+
+int countComponents(int n, vector<vector<int>>& edges) {
+    vector<vector<int>> graph(n + 1, vector<int>());
+
+    for (int i = 0; i < edges.size(); i++) {
+        graph[edges[i][0]].push_back(edges[i][1]);
+        graph[edges[i][1]].push_back(edges[i][0]);
+    }
+
+    vector<bool> seen(n, false);
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (!seen[i]) {
+            ans++;
+            dfs(i, graph, seen);
         }
     }
 
-    int countComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> graph(n + 1, vector<int>());
-
-        for (int i = 0; i < edges.size(); i++) {
-            graph[edges[i][0]].push_back(edges[i][1]);
-            graph[edges[i][1]].push_back(edges[i][0]);
-        }
-
-        vector<bool> seen(n, false);
-        int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (!seen[i]) {
-                ans++;
-                dfs(i, graph, seen);
-            }
-        }
-
-        return ans;
-    }
-};
+    return ans;
+}
